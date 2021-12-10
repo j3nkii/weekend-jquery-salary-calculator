89315a8fbad364f6f586ebf06bd709dead7357c6
Jacob Larson <jacob.larson55112@gmail.com>
2021-12-10 16:57:35 -0600

    able to append to DOM

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //Script for index
-
+let emloyeeDataBase = [];
 
 $(ready);
 function ready(){
     console.log('readyIsGo');
+    $(document).on('submit', '#employeeInputForm', addEmployeeToDataBase)
+}
+
+function addEmployeeToDataBase(){
+    event.preventDefault()
+    //create new object and push to database
+    emloyeeDataBase.push({
+        firstName: $('#firsNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        employeeNumber: $('#employeeNumberInput').val(),
+        jobTitle: $('#jobTitleInput').val(),
+        annualSalary: $('#annualSalaryInput').val(),
+    })
+    console.log(emloyeeDataBase);
+    //append to DOM
+    $('#tableBody').append(
+    `<tr>
+        <td>${$('#firsNameInput').val()}</td>
+        <td>${$('#lastNameInput').val()}</td>
+        <td>${$('#employeeNumberInput').val()}</td>
+        <td>${$('#jobTitleInput').val()}</td>
+        <td>${$('#annualSalaryInput').val()}</td>
+        <td> :trash: </td>
+    </tr>`)
+    //update price with potential to trigger red event
 }
