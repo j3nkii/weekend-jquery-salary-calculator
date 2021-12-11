91018562cd6e1970715cb2177555fa78ef3279ea
Jacob Larson <jacob.larson55112@gmail.com>
2021-12-11 13:28:57 -0600

    fixed bugs

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ function ready(){
     $(document).on('click', '.toggleEditMode', editEmployee);                 //Toggles to 'edit mode'
     $(document).on('click', '.editButton', editEmployee);                    //Attempting to make edits
     displayEmployeeToDom();                                                 //load database to DOM on initiation
-    console.log('ON LOAD');
-    console.table(employeeDataBase);
 }
 
 
@@ -62,6 +60,9 @@ function addEmployeeToDataBase(){
  There is also a .data() method being added to the <TR> tag. it utilizes the employee number
     as there should be no duplicates of these, and attached to it, is the salary of the 
     employee. This will be used to reduce the totalAnnualSalary in the event of employee removal.
+
+    could rebuild function to take an object as a paramet and use a ternary to set the 
+    iterated array to one with only the object or the globaldatabase, which will not pass a parameter on load
  */
 function displayEmployeeToDom(bool){
     let loadProgress = 0;
@@ -89,20 +90,14 @@ function displayEmployeeToDom(bool){
             overBudgetEvent();
             loadProgress++
             $('.inputFeild').val('');
-            console.log('ON DISPLAY');
-            console.table(employeeDataBase);
         if(loadedPage){
             break;
         } else {
-            //console.log(`Loading ${loadProgress} of ${employeeDataBase.length}`); 
             loadedPage = loadProgress === employeeDataBase.length 
                 ? true
                 : false;
         }
     }
-    //loadedPage && loadProgress > 1
-    //    ? console.log('Page loaded!')
-     //   : false;
 }
 
 
@@ -180,16 +175,25 @@ function editEmployee(){
                 currentEmployee = employee
             }
         }
-        loadedPage = false;
-        $('#tableBody').empty();
-        displayEmployeeToDom();
-        console.log('onEdit',employeeDataBase)
+        $(this).parent()
+        .siblings(".employeeSalary")
+        .empty()
+        .text(`${formatter.format(currentEmployee.annualSalary)}`);
+    $(this).parent()
+        .empty()
+        .append(`<button class="removalButton"><svg 
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg></button>
+                <button class="toggleEditMode"><svg 
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                    <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                    <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                </svg></button>`);
         overBudgetEvent();
-        loadedPage = true;
         editorMode = false;
     }
-    console.log('ON ADD');
-    console.table(employeeDataBase);
 }
 
 
