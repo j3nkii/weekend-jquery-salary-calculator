e659da28081886c26227449b22f7e81daf0930b1
Jacob Larson <jacob.larson55112@gmail.com>
2021-12-12 12:03:48 -0600

    found and fixed more budgetEvent problems

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function ready(){
     $(document).on('click', '.toggleEditMode', editEmployee);                 //Toggles to 'edit mode' which switcher table text to <inputs>
     $(document).on('click', '.editButton', editEmployee);                    //Used to edit employees in table when pressing the commit button
     displayEmployeeToDom();                                                 //load database to DOM on initiation
+    budgetEvent();
 }
 
 
@@ -34,11 +35,15 @@ This function takes in values from DOM, adds them
      to the database, and then utilized the 
      display function, passing the object
      to be rendered to DOM
-
-Could also use a for loop to search through existing ID's to prevent duplicates
  */
 function addEmployeeToDataBase(){
     event.preventDefault();
+    for(let employee of employeeDataBase){
+        if(Number($('#employeeNumberInput').val()) === Number(employee.employeeNumber)){
+            window.alert('User Already Exists')
+            return;
+        }
+    }
     let additionalEmployee = {
         firstName: $('#firsNameInput').val(),
         lastName: $('#lastNameInput').val(),
@@ -48,6 +53,7 @@ function addEmployeeToDataBase(){
     }
     employeeDataBase.push(additionalEmployee);
     displayEmployeeToDom(additionalEmployee);
+    budgetEvent(additionalEmployee.annualSalary);
 }
 
 
@@ -91,7 +97,6 @@ function displayEmployeeToDom(obj){
             </tr>`);
             $('.inputFeild').val('');
     }
-    budgetEvent();
 }
 
 
@@ -106,6 +111,9 @@ function displayEmployeeToDom(obj){
  It also now adds all salaries in database and appends to the DOM,
     another reason it should be used in any other function that
     interacts with salaries
+When calling function it should always take a parameter,
+    otherwise it will add up everthing inside databse,
+    which it does on pageload
  */
 function budgetEvent(num){
     if(num === undefined){
@@ -114,9 +122,8 @@ function budgetEvent(num){
         }
     } else {
         totalAnnualSalary += num;
-        console.log(totalAnnualSalary , num);
     }
-    let monthly = Math.round(totalAnnualSalary /12)
+    let monthly = Math.round(totalAnnualSalary / 12)
     if(monthly > 20000){
         $("footer").css("background-color", "red");
         $("footer").css("color", "white");
@@ -129,21 +136,27 @@ function budgetEvent(num){
 
 
 
-/**NEEDS COMMENTS
+/**
+Function first calls dats attacheched to the employeesalary class,
+    using it to call our budget even which will update the dom with monthly cost
+then it loops through database searhing for matching ID numbers, 
+    as there should be no duplicates, and deletes the employee from databse
+)
  */
 function removeEmployee(){
+    budgetEvent(-$(this)
+        .parent().
+        siblings('td.employeeSalary')
+        .data('salary'));
     $(this).parents('tr').remove();
-    console.log($(this).parent().siblings('td.employeeSalary').data('salary'));
-    let index = -1;
-    for(let employee of employeeDataBase){
-        index++;
-        if(Number(employee.employeeNumber) === $(this).parent()
+    for(let index in employeeDataBase){
+        if(Number(employeeDataBase[index].employeeNumber) === $(this).parent()
             .siblings('td.employeeNumber')
             .data('employeenumber')){
             employeeDataBase.splice(index,  1);
         }
     }
-    budgetEvent(-$(this).parent().siblings('td.employeeSalary').data('salary'));
+    
 }
 
 
@@ -159,55 +172,54 @@ function removeEmployee(){
     the remove and edit buttons from before
  The last if, just throws an error for the user.
 
- In the future, this could be able to edit all inputs, and also hold the values for input as to not get mixed up 
+ In the future, this could be able to edit all inputs. 
  */
 function editEmployee(){
+    let oldSalary = $(this).parent()
+        .siblings('.employeeSalary')
+        .data('salary')
     if(!editorMode && $(this).attr('class') === 'toggleEditMode'){
-    $(this).parent()
-        .siblings(".employeeSalary")
-        .empty()
-        .append(`<input id="editEmployeeFromDOM" type="number" value="${
-            $(this).parent()
-            .siblings('.employeeSalary')
-            .data('salary')}">`);
-    $(this).parent()
-        .empty()
-        .append(`<button class="editButton">Commit</button>`);
-        console.log('now able to edit');
-    //Toggle for function to handle edit / commit buttons
-    editorMode = true;
+        $(this).parent()
+            .siblings(".employeeSalary")
+            .empty()
+            .append(`<input id="editEmployeeFromDOM" type="number" value="${oldSalary}">`);
+        $(this).parent()
+            .empty()
+            .append(`<button class="editButton">Commit</button>`);
+            console.log('now able to edit');
+        editorMode = true;
     } else if(editorMode && $(this).attr('class') === 'editButton'){
         let currentEmployee;
-        let updatedEmployee = Number($('#editEmployeeFromDOM').val());
+        let updatedSalary = Number($('#editEmployeeFromDOM').val());
         let employeeNumber = $(this).parent()
             .siblings('td.employeeNumber')
             .data('employeenumber');
-        for(let employee of employeeDataBase){
+        for(let employee of employeeDataBase){ //searching database to update info, since here, using values below
             if(employeeNumber === Number(employee.employeeNumber)){
                 currentEmployee = employee;
-                currentEmployee.annualSalary = updatedEmployee;
+                currentEmployee.annualSalary = updatedSalary;
             }
         }
         $(this).parent()
-        .siblings(".employeeSalary")
-        .empty()
-        .text(`${formatter.format(currentEmployee.annualSalary)}`);
+            .siblings(".employeeSalary")
+            .empty()
+            .text(`${formatter.format(currentEmployee.annualSalary)}`);
         $(this).parent()
-        .siblings(".employeeSalary")
-        .data('salary', currentEmployee.annualSalary)
+            .siblings(".employeeSalary")
+            .data('salary', currentEmployee.annualSalary)
         $(this).parent()
-        .empty()
-        .append(`<button class="removalButton"><svg 
-                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg></button>
-                <button class="toggleEditMode"><svg 
-                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
-                    <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
-                    <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
-                </svg></button>`);
-        budgetEvent();
+            .empty()
+            .append(`<button class="removalButton"><svg 
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg></button>
+                    <button class="toggleEditMode"><svg 
+                        xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+                        <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+                        <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
+                    </svg></button>`);
+            budgetEvent(Math.abs(oldSalary - updatedSalary));
         editorMode = false;
     } else {
         window.alert('One at a Time!');
